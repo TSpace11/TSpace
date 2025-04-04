d908eb8f90f49697b4466a524a28bdaed59daa5f
TSpace11 <tspace04022025@gmail.com>
2025-04-04 23:12:22 +0800

    a

--- TSpace java.js
+++ TSpace java.js
@@ -49,12 +49,12 @@ pageLinks.forEach((link) => {
     }); 
 }); 
 
-/* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
   
-  /* Set the width of the sidebar to 0 (hide it) */
   function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
   }
