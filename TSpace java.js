391df49f12f493480448b7a96a66a32b452096a3
TSpace11 <tspace04022025@gmail.com>
2025-04-09 17:21:36 +0800

    TSpace

--- TSpace java.js
+++ TSpace java.js
@@ -48,3 +48,15 @@ pageLinks.forEach((link) => {
         } 
     }); 
 }); 
+
+function side__open() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+}
+function side__close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
